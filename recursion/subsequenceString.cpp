26a7f2041cdac2cf04307fcfcf597ad45e856ddb
smitambalia <smit.ambalia@gmail.com>
2024-06-23 15:47:41 +0530

    Change : Stored sub sequence into vector instead of just printing

--- recursion/subsequenceString.cpp
+++ recursion/subsequenceString.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 /* 
@@ -8,31 +8,37 @@ using namespace std;
     We will apply , Include and Exclude pattern for this problem
 
  */
-void findSubsequenceString(string str, string outputString, int index) {
+void findSubsequenceString(string str, string outputString, int index,vector<string>& ans) {
 
     // base case 
     if(index >= str.length()) {
-        cout << "--> " << outputString << endl ;
+        ans.push_back(outputString);
         return ;
     }
 
     // exclude the character 
-    findSubsequenceString(str,outputString,index+1);
+    findSubsequenceString(str,outputString,index+1,ans);
 
     // include the character 
     char extractCharFromIndex = str[index];
     // include the extracted character into the output string 
     outputString.push_back(extractCharFromIndex);
 
-    findSubsequenceString(str,outputString,index+1);
+    findSubsequenceString(str,outputString,index+1,ans);
 }
 
 
 int main() {
 
-    string subsequenceString = "xyz";
+    string subsequenceString = "xy";
     string outputString = "";
     int index = 0;
-    findSubsequenceString(subsequenceString,outputString,index);
+    vector<string> ans;
+    findSubsequenceString(subsequenceString,outputString,index,ans);
+
+    // printing sub sequence string
+    for(string subsequence : ans) {
+        cout << "-> " << subsequence << endl;
+    }
     return 0;
 }
