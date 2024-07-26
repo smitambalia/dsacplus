2bf333f895273d633aaafb1f4c98ac313f1ca55b
smitambalia <smit.ambalia@gmail.com>
2024-07-26 22:41:26 +0530

    Adding Linked List example

---
+++ LinkedList/Sample.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        this->data = value;
+        this->next = NULL;
+    }
+   
+};
+
+void insertAtTail(int value, Node* &head, Node* &tail) {
+    Node* newNode = new Node(value);
+    if(head == NULL && tail== NULL) {
+        head = newNode;
+        tail = newNode;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+
+}
+void insertAtHead(int value,Node* &head, Node* &tail) {
+    Node* newNode = new Node(value);
+
+    if(head == NULL && tail== NULL) {
+        head = newNode;
+        tail = newNode;
+    }
+
+    newNode->next = head;
+    head = newNode;
+}
+int findLengthOfLL(Node* &head) {
+
+    Node* temp = head;
+    int len;
+    while (temp != NULL)
+    {   
+        temp = temp->next;
+        len++;
+        /* code */
+    }
+
+    return len;
+    
+}
+
+void insertAtPotion(int value, Node* &head, Node* &tail,int position) {
+    int length = findLengthOfLL(head);
+
+    if(position == 1) {
+        insertAtHead(value,head,tail);
+    }
+    if(position > length) {
+        insertAtTail(value,head,tail);
+    }
+
+    Node* temp = head;
+    for(int i=1 ; i <= position -2 ; i++) {
+        temp = temp->next;
+    }
+    Node* newNode = new Node(value);
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+}
+
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+bool search(int target , Node* &head) {
+    Node* temp = head;
+
+    while (temp != NULL) 
+    {
+        /* code */
+        if(temp->data == target) return true;
+
+        temp = temp->next;
+    }
+
+    return false;
+    
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    insertAtTail(10,head,tail);
+    insertAtTail(20,head,tail);
+    insertAtTail(30,head,tail);
+    insertAtHead(35,head,tail);
+
+    // display the nodes
+
+    print(head);
+
+    insertAtPotion(55,head,tail,10);
+    print(head);
+
+
+    return 0;
+}
--- recursion/checkForSorting.cpp
+++ recursion/checkForSorting.cpp
@@ -9,16 +9,25 @@ bool checkSortingOfArray(int arr[], int size, int index)
     if (index >= size)
         return true;
 
-    if (arr[index] > arr[index - 1])
+    bool currAns = false , recusiveAns = false;
+
+    currAns = (arr[index + 1] > arr[index]) ? true : false ;
+
+    recusiveAns = checkSortingOfArray(arr,size, index+ 1);
+
+    if(currAns && recusiveAns) return true;
+    else return false;
+
+   /*  if (arr[index] > arr[index - 1])
         return checkSortingOfArray(arr, size, index + 1);
     else
-        return false;
+        return false; */
 }
 
 int main()
 {
 
-    int arr[] = {10, 20, 30, 60, 50};
+    int arr[] = {10, 20, 30, 90, 70};
     bool isArraySorted = checkSortingOfArray(arr, 5, 1);
     if (isArraySorted)
         cout << "Array is Sorted"  << endl;
