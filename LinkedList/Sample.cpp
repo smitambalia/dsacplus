325d89892554182208720a80fcdc7a9e44cfb878
smitambalia <smit.ambalia@gmail.com>
2024-11-16 11:23:10 +0530

    Added Palindrom for the string in LinkedList
    Added Palindrome for the string in LinkedList

--- LinkedList/Sample.cpp
+++ LinkedList/Sample.cpp
@@ -103,22 +103,92 @@ bool search(int target, Node *&head)
     }
 
     return false;
+}   
+
+Node* reverseLinkedListWithoutRecursion(Node *&head) {
+
+    Node *prev = NULL;
+    Node *current = head;
+
+    // repeate the process until
+    while(current != NULL) {
+        Node *forward = current->next;
+        current->next = prev;
+        prev = current;
+        current = forward;
+    }
+
+    return prev;
+
 }
 
-int findTheMiddleValue(Node *&head) {
+ 
+
+/**
+ * find the middle node with fast and slow Node pointer
+ * slow pointer increases one node at a time
+ * fast pointer node increases two times at a time
+ */
+Node* findTheMiddleValue(Node *&head) {
     Node* slow = head;
     Node* fast = head;
+    Node* beforeSlow = NULL;
 
     while(fast != NULL) {
         fast = fast->next;
-        if( fast != NULL) {
+        if(fast != NULL) {
             fast = fast->next;
+            beforeSlow = slow;
             slow = slow->next;
         }
     }
-    return slow->data;
+    return beforeSlow;
 }
 
+
+/**
+ * 40 35 20 10 30 
+ * middle node 20 
+ * make two linked-list
+ * reverse the 2nd one 
+ * returns true if both linked lists matches
+ */
+bool isPalindrom(Node* &head) {
+    // palindrom when there is no node 
+    if(head == NULL) return true;
+    // palindrom when there only one node
+    if(head->next == NULL) return true;
+
+    print(head);
+    Node* middleNode = findTheMiddleValue(head);
+    cout << middleNode->data << " " << endl;
+
+    // create a secound linked list 
+    Node* headForSecoundNode = middleNode->next;
+
+    // make first linked list by ponting last node of next to NULL
+    middleNode->next = NULL;
+
+    //  reverse the secound linked list
+    headForSecoundNode = reverseLinkedListWithoutRecursion(headForSecoundNode);
+    print(headForSecoundNode);
+    print(head);
+
+    Node* headForFirstNode = head;
+
+    while (headForFirstNode != NULL && headForSecoundNode !=NULL) {
+
+        if(headForFirstNode->data != headForSecoundNode->data) {
+            return false;
+        }
+        else {
+            headForFirstNode = headForFirstNode->next;
+            headForSecoundNode= headForSecoundNode->next;
+        }
+    }
+    return true;
+   
+}
 
 int main()
 {
@@ -126,20 +196,22 @@ int main()
     Node *head = NULL;
     Node *tail = NULL;
 
-    insertAtTail(20, head, tail);
-    insertAtTail(10, head, tail);
-    insertAtTail(30, head, tail);
-    insertAtHead(35, head, tail);
-    // insertAtTail(40, head, tail);
+    insertAtTail(1, head, tail);
+    insertAtTail(2, head, tail);
+    insertAtTail(3, head, tail);
+    insertAtTail(2, head, tail);
+    insertAtTail(1, head, tail);
 
 
-    // display the nodes
+    bool linkeListIsPalindrom = isPalindrom(head);
+    if(linkeListIsPalindrom) {
+        cout << "LinkedList is palindrom!!" << endl ;
+    }
+    else {
+        cout << "LinkedList is not palindrom!!" << endl ;
 
-    print(head);
-    int middleValue = findTheMiddleValue(head);
-    cout << middleValue << " " << endl;
-    // insertAtPotion(55, head, tail, 10);
-    // print(head);
-
+    }
+    
+    // find the palindrom 
     return 0;
 }
